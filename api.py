35a4823f63bb50389a225a629dcb30bcb59c9410
Cody <codylmathisen@gmail.com>
2015-06-26 00:24:13 -0500

    removed 2 hr time adjustment, added memcache to api call

--- api.py
+++ api.py
@@ -9,6 +9,7 @@ from packages import gviz_api
 
 from google.appengine.api import urlfetch
 from google.appengine.ext import ndb
+from google.appengine.api import memcache
 
 import base
 
@@ -33,109 +34,112 @@ class ApiChallengeHandler(webapp2.RequestHandler):
 		
 
 class ApiHandler(webapp2.RequestHandler):
-    def get(self, id):
-        if not id.isdigit():
-            self.response.write('Invalid ID')
-            self.error(500)
-            return
-        
-        challenge = Challenge.query(Challenge.num == int(id)).get()
-        description = {
-            'updated': ('datetime', 'Updated'),
-            'value': ('number', 'Value')
-        }
-        
-        data = []
-        i = 0
-        counter = 0
-        values = []
-        for point in challenge.datapoints:
-            counter += point.increment
-            values.append(point.value)
-            if (i % 4) == 0:
-                data.append({
-                    'updated': point.updated.replace(minute=0, second=0, microsecond=0),
-                    'value': counter if challenge.type == 'counter' else sum(values)/len(values)
-                })
-                counter = 0
-                values = []
-            i += 1
-            
-        data_table = gviz_api.DataTable(description)
-        data_table.LoadData(data)
-        json = data_table.ToJSon()
-        self.response.write(json)
-        
+	# change this to use slug
+	def get(self, id):
+		if not id.isdigit():
+			self.response.write('Invalid ID')
+			self.error(500)
+			return
+		challenge = memcache.get('challenge_%s' % id)
+		if challenge is None:
+			challenge = Challenge.query(Challenge.num == int(id)).get()
+			memcache.set('challenge_%s' % id, challenge, 60)
+		description = {
+			'updated': ('datetime', 'Updated'),
+			'value': ('number', 'Value')
+		}
+		
+		data = []
+		i = 0
+		counter = 0
+		values = []
+		for point in challenge.datapoints:
+			counter += point.increment
+			values.append(point.value)
+			if (i % 4) == 0:
+				data.append({
+					'updated': point.updated.replace(minute=0, second=0, microsecond=0),
+					'value': counter if challenge.type == 'counter' else sum(values)/len(values)
+				})
+				counter = 0
+				values = []
+			i += 1
+			
+		data_table = gviz_api.DataTable(description)
+		data_table.LoadData(data)
+		json = data_table.ToJSon()
+		self.response.write(json)
+		
 
 class ApiHandler_raw_json(webapp2.RequestHandler):
-    def get(self, id):
-        if not id.isdigit():
-            self.response.write('Invalid ID')
-            self.error(500)
-            return
-            
-        challenge = Challenge.query(Challenge.num == int(id)).get()
-        
-        if challenge is None:
-            self.response.write('None')
-            self.error(404)
-            return
-        data = dict();
-        cols = [{
-            'label': 'Time',
-            'type': 'datetime'
-        }, {
-            'label': 'Values',
-            'type': 'number'
-        }]
-        data['increment'] = {
-            'cols': cols,
-            'rows': []
-        }
-        data['total'] = {
-            'cols': cols,
-            'rows': []
-        }
-        # formatting: https://developers.google.com/chart/interactive/docs/reference#dataparam
-        # example: https://developers.google.com/chart/interactive/docs/php_example
-        total = 0
-        i = 0
-        hours = 0
-        counter = 0
-        finished = 0
-        for point in challenge.datapoints:
-            if (i % 4) == 0:
-                data['increment']['rows'].append({
-                    'c': [
-                        {'v': point.get_fdate_js(), 'f': 'test'},
-                        {'v': counter}
-                    ]
-                })
-                data['total']['rows'].append({
-                    'c': [
-                        {'v': hours},
-                        {'v': total}
-                    ]
-                })
-                counter = point.increment
-                hours += 1
-                finished = 1
-            else:
-                counter += point.increment
-                finished = 0
-            i += 1
-            total += point.increment
-        if not finished:
-            data['increment']['rows'].append({
-                'c': [
-                    {'v': hours},
-                    {'v': counter}
-                ]
-            })
-            data['total']['rows'].append({
-                'c': [
-                    {'v': hours},
-                    {'v': total}
-                ]
-            })
-        self.response.write(json.dumps(data['increment']))
+	def get(self, id):
+		if not id.isdigit():
+			self.response.write('Invalid ID')
+			self.error(500)
+			return
+			
+		challenge = Challenge.query(Challenge.num == int(id)).get()
+		
+		if challenge is None:
+			self.response.write('None')
+			self.error(404)
+			return
+		data = dict();
+		cols = [{
+			'label': 'Time',
+			'type': 'datetime'
+		}, {
+			'label': 'Values',
+			'type': 'number'
+		}]
+		data['increment'] = {
+			'cols': cols,
+			'rows': []
+		}
+		data['total'] = {
+			'cols': cols,
+			'rows': []
+		}
+		# formatting: https://developers.google.com/chart/interactive/docs/reference#dataparam
+		# example: https://developers.google.com/chart/interactive/docs/php_example
+		total = 0
+		i = 0
+		hours = 0
+		counter = 0
+		finished = 0
+		for point in challenge.datapoints:
+			if (i % 4) == 0:
+				data['increment']['rows'].append({
+					'c': [
+						{'v': point.get_fdate_js(), 'f': 'test'},
+						{'v': counter}
+					]
+				})
+				data['total']['rows'].append({
+					'c': [
+						{'v': hours},
+						{'v': total}
+					]
+				})
+				counter = point.increment
+				hours += 1
+				finished = 1
+			else:
+				counter += point.increment
+				finished = 0
+			i += 1
+			total += point.increment
+		if not finished:
+			data['increment']['rows'].append({
+				'c': [
+					{'v': hours},
+					{'v': counter}
+				]
+			})
+			data['total']['rows'].append({
+				'c': [
+					{'v': hours},
+					{'v': total}
+				]
+			})
+		self.response.write(json.dumps(data['increment']))
--- config.py
+++ config.py
@@ -7,7 +7,7 @@ URL_API_EVOLVE_CHALLENGE = 'http://api.4v1game.net/challenge/'
 DEFAULT_CHALLENGE_KEY = 'default_challenge'
 DEFAULT_CHALLENGE_DURATION = datetime.timedelta(days=3)
 DEFAULT_CHALLENGE_POST_DELAY = datetime.timedelta(hours=1)
-API_DATETIME_ADJUST = datetime.timedelta(hours=2)
+API_DATETIME_ADJUST = datetime.timedelta(hours=0)
 
 MEMCACHE_TIME = 60*15
 
